5e0deb8778a6b1046a40f3760c123eb1b53c88f6
Jakub Argasiński <argasek@gmail.com>
2010-09-18 20:13:37 +0200

    Zalążek aplikacji opartej na Zend Framework, w tym: struktura katalogów, moduł domyślny, podstawowe kontrolery indeksu i błędów wraz z odpowiednimi układami, licencja

---
+++ library/void/Void/Exception.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Void 
+ *
+ * LICENSE
+ *
+ * This source file is subject to the Simplified BSD License that is
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://void.jogger.pl/files/void-bsd-license.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to argasek@gmail.com so I can send you a copy immediately.
+ *
+ * @category   Void
+ * @package    Void
+ * @copyright  Copyright (c) 2010 Jakub Argasiński (argasek@gmail.com)
+ * @license    http://void.jogger.pl/files/void-bsd-license.txt     New BSD License
+ * @version    $Id: Exception.php 3 2010-05-12 21:27:11Z argasek $
+ */
+
+/**
+ * Generic exception
+ * 
+ * @author     Jakub Argasiński <argasek@gmail.com>
+ *
+ */
+
+class Void_Exception extends Zend_Exception {}
