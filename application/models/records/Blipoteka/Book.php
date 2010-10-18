b6b1f981c905d9096a67e0c855fe26098e7c731d
Jakub Argasiński <argasek@gmail.com>
2010-10-18 20:11:55 +0200

    Add some basic book entity constraints and corresponding unit tests.

--- application/models/records/Blipoteka/Book.php
+++ application/models/records/Blipoteka/Book.php
@@ -125,7 +125,7 @@ class Blipoteka_Book extends Doctrine_Record {
 		// We assume each book has at least one author and we consider him/her a main (default) author.
 		// If this author gets deleted, all books by him/her are deleted as well.
 		$this->hasOne('Blipoteka_Author as author', array('local' => 'author_id', 'foreign' => 'author_id', 'onUpdate' => 'CASCADE', 'onDelete' => 'CASCADE'));
-		// Book may have many authors
+		// Book may have many additional authors
 		$this->hasMany('Blipoteka_Author as authors', array(
 			'local' => 'book_id',
 			'foreign' => 'author_id',
@@ -146,6 +146,40 @@ class Blipoteka_Book extends Doctrine_Record {
 		// If the publisher gets deleted, all books published by him/her are deleted as well.
 		$this->hasOne('Blipoteka_Publisher as publisher', array('local' => 'publisher_id', 'foreign' => 'publisher_id', 'onUpdate' => 'CASCADE', 'onDelete' => 'CASCADE'));
 		$this->actAs('Timestampable', array('updated' => array('disabled' => true)));
+	}
+
+	/**
+	 * Check if saved data is right
+	 * @see Doctrine_Record::preSave()
+	 */
+	public function preSave($event) {
+		$invoker = $event->getInvoker();
+
+		// One cannot be a current owner and a holder of a book at the same time.
+		if ($invoker->owner_id !== null && $invoker->owner_id === $invoker->holder_id) {
+			throw new Doctrine_Record_Exception("Owner and holder of a book can't be the same person", Doctrine_Core::ERR_CONSTRAINT);
+		}
+	}
+
+	/**
+	 * Preparation of record
+	 * @see Doctrine_Record::preInsert()
+	 */
+	public function preInsert($event) {
+		$invoker = $event->getInvoker();
+
+		// A book has to be added by some user
+		if ($invoker->user_id === null) {
+			throw new Doctrine_Record_Exception("Tried to add a book not bound to any user", Doctrine_Core::ERR_CONSTRAINT);
+		}
+
+		// At the moment of insertion, a book cannot be held by anyone
+		if ($invoker->holder_id !== null) {
+			throw new Doctrine_Record_Exception("Tried to add a book being held by somebody", Doctrine_Core::ERR_CONSTRAINT);
+		}
+
+		// When a book is added to a user's pool, he or she becomes an owner automatically
+		$invoker->owner_id = $invoker->user_id;
 	}
 
 }
---
+++ tests/Blipoteka/BookTest.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Blipoteka.pl
+ *
+ * LICENSE
+ *
+ * This source file is subject to the Simplified BSD License that is
+ * bundled with this package in the file docs/LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://blipoteka.pl/license
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to blipoteka@gmail.com so we can send you a copy immediately.
+ *
+ * @category   Blipoteka
+ * @package    Blipoteka_Tests
+ * @copyright  Copyright (c) 2010 Jakub Argasiński (argasek@gmail.com)
+ * @license    http://blipoteka.pl/license Simplified BSD License
+ */
+
+/**
+ * Book entity test case
+ *
+ * @author Jakub Argasiński <argasek@gmail.com>
+ *
+ */
+class Blipoteka_BookTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @var Blipoteka_Book
+	 */
+	private $book;
+
+	/**
+	 * Set up an example book with minimum information required
+	 * @see PHPUnit_Framework_TestCase::setUp()
+	 */
+	protected function setUp() {
+		$this->book = new Blipoteka_Book();
+		$this->book->title = 'Przykładowa, bardzo interesująca książka';
+		$this->book->author_id = 1;
+		$this->book->city_id = 756135;
+		$this->book->publisher_id = 1;
+		$this->book->user_id = 1;
+	}
+
+	/**
+	 * We expect user_id to be not NULL when inserting.
+	 * @expectedException Doctrine_Record_Exception
+	 */
+	public function testPreInsertUserIdNotNull() {
+		$this->book->user_id = null;
+		$this->book->save();
+	}
+
+	/**
+	 * We expect holder_id to be NULL when inserting.
+	 * @expectedException Doctrine_Record_Exception
+	 */
+	public function testPreInsertHolderIdNull() {
+		$this->book->holder_id = 2;
+		$this->book->save();
+	}
+
+	/**
+	 * We expect owner_id to be set to the same value as user_id when inserting (and after insertion, too).
+	 * @depends testPreInsertUserIdNotNull
+	 */
+	public function testPreInsertOwnerEqualsUser() {
+		$this->book->save();
+		$this->assertEquals($this->book->user_id, $this->book->owner_id, 'A book after insertion should have the same user and owner');
+	}
+
+	/**
+	 * We expect holder_id to be not the same as owner_id (unless owner_id is NULL) when inserting or updating.
+	 * @expectedException Doctrine_Record_Exception
+	 */
+	public function testPreSaveNotNullOwnerCannotEqualHolder() {
+		$this->book->holder_id = 1;
+		$this->book->save();
+	}
+
+	/**
+	 * We expect holder_id can be the same as owner_id when updating only when owner_id is NULL.
+	 * @depends testPreInsertOwnerEqualsUser
+	 */
+	public function testPreSaveNullOwnerCanEqualHolder() {
+		$this->book->save();
+		$this->book->owner_id = null;
+		$this->book->holder_id = null;
+		$this->book->save();
+		$this->assertNull($this->book->owner_id);
+		$this->assertNull($this->book->holder_id);
+	}
+
+}
