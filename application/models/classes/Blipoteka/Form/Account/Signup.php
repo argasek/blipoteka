af7ef7d374b6688663f7688a002eefdc76033cbe
Jakub Argasiński <argasek@gmail.com>
2010-12-04 23:17:41 +0100

    Add Blipoteka_Form_Account_Signup and Blipoteka_Form_Account_Signup skeletons.

---
+++ application/models/classes/Blipoteka/Form/Account/Signup.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Blipoteka.pl
+ *
+ * LICENSE
+ *
+ * This source file is subject to the Simplified BSD License
+ * that is bundled with this package in the file docs/LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://blipoteka.pl/license
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to blipoteka@gmail.com so we can send you a copy immediately.
+ *
+ * @category   Blipoteka
+ * @package    Blipoteka_Form_Account
+ * @copyright  Copyright (c) 2010 Jakub Argasiński (argasek@gmail.com)
+ * @license    http://blipoteka.pl/license Simplified BSD License
+ */
+
+/**
+ * Signup for a new account form
+ *
+ * @author Jakub Argasiński <argasek@gmail.com>
+ *
+ */
+class Blipoteka_Form_Account_Signup extends Zend_Form {
+
+	public function init() {
+		$this->setMethod('post');
+	}
+}
