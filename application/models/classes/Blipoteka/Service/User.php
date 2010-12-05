add4f409549eff9ed5eafa6d7a69e6d390aa19f9
Jakub Argasiński <argasek@gmail.com>
2010-12-05 17:53:51 +0100

    Update setPassword method not to change password (store it in the record only); use a new updatePassword method from now on. Add createUser, getDefaultCity and getDefaultUserName methods.

--- application/models/classes/Blipoteka/Service/User.php
+++ application/models/classes/Blipoteka/Service/User.php
@@ -27,6 +27,9 @@
  *
  */
 class Blipoteka_Service_User extends Blipoteka_Service {
+	const DEFAULT_USER_NAME = 'Koziołek Matołek';
+	const DEFAULT_CITY_NAME = 'Pacanów';
+
 	/**
 	 * Class of the record this service applies to
 	 * @var string
@@ -58,10 +61,11 @@ class Blipoteka_Service_User extends Blipoteka_Service {
 
 	/**
 	 * Process given user password hashing/salting algorithms
-	 * if provided and store it in a database.
+	 * if provided and store it in a record.
 	 *
 	 * @param string $password
 	 * @param Blipoteka_User $user
+	 * @return Blipoteka_Service_User
 	 */
 	public function setPassword($password, Blipoteka_User $user) {
 		if ($this->_authAdapter instanceof Void_Auth_Adapter_Interface) {
@@ -69,7 +73,67 @@ class Blipoteka_Service_User extends Blipoteka_Service {
 			$password = $this->_authAdapter->getTreatedCredential();
 		}
 		$user->password = $password;
+		return $this;
+	}
+
+	/**
+	 * Process given user password hashing/salting algorithms
+	 * if provided and update user's password.
+	 *
+	 * @param string $password
+	 * @param Blipoteka_User $user
+	 * @return Blipoteka_Service_User
+	 */
+	public function updatePassword($password, Blipoteka_User $user) {
+		$this->setPassword($password, $user);
 		$user->save();
+		return $this;
+	}
+
+	/**
+	 * Create user account with reasonable default values.
+	 *
+	 * @param Blipoteka_User $user
+	 * @return Blipoteka_Service_User
+	 */
+	public function createUser(Blipoteka_User $user, Blipoteka_Form_Account_Signup $form) {
+		$user->blip = $form->getValue('login');
+		$user->email = $form->getValue('email');
+		$this->setPassword($form->getValue('password'), $user);
+		// If user name not provided, use default
+		if ($user->name === null) {
+			$user->name = $this->getDefaultUserName();
+		}
+		// If user city not provided, use default
+		if ($user->city === null) {
+			$user->city = $this->getDefaultCity();
+		}
+		// Create user's account.
+		$user->save();
+
+		return $this;
+	}
+
+	/**
+	 * Return default city.
+	 *
+	 * @todo This should be set up by some resource
+	 * @return string
+	 */
+	protected function getDefaultCity() {
+		$name = self::DEFAULT_CITY_NAME;
+		return Doctrine_Query::getTable('Blipoteka_City')->findOneByName($name);
+	}
+
+	/**
+	 * Return default user name.
+	 *
+	 * @todo This should be set up by some resource
+	 * @return string
+	 */
+	protected function getDefaultUserName() {
+		$name = self::DEFAULT_USER_NAME;
+		return $name;
 	}
 
 }
